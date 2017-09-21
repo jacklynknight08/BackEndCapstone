49b0f03b8e723a67cd460a2595a15043918f5f11
jacklynknight08 <jnk08@outlook.com>
2017-09-20 19:53:12 -0500

    Setting up model for client profile

--- Models/ManageViewModels/IndexViewModel.cs
+++ Models/ManageViewModels/IndexViewModel.cs
@@ -8,6 +8,8 @@ namespace BackEndCapstone.Models.ManageViewModels
 {
     public class IndexViewModel
     {
+        // Set an instance of an application user, inherits from Identity
+        public ApplicationUser AppUser { get; set; }
         public bool HasPassword { get; set; }
 
         public IList<UserLoginInfo> Logins { get; set; }
@@ -17,5 +19,14 @@ namespace BackEndCapstone.Models.ManageViewModels
         public bool TwoFactor { get; set; }
 
         public bool BrowserRemembered { get; set; }
+
+        // Give appuser access to appointments
+        public ICollection<Appointment> Appointments { get; set; }
+
+        // IndexViewModel will be used in ManageController for Index.cshtml and Edit.cshtml
+        public IndexViewModel()
+        {
+            AppUser = new ApplicationUser();
+        }
     }
 }
