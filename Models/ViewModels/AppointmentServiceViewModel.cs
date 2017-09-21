997dc1847d9ad34715b121f7606fac0fb65dfee4
jacklynknight08 <jnk08@outlook.com>
2017-09-20 21:00:47 -0500

    Altered view model

--- Models/ViewModels/AppointmentServiceViewModel.cs
+++ Models/ViewModels/AppointmentServiceViewModel.cs
@@ -10,12 +10,12 @@ namespace BackEndCapstone.Models.ViewModels
     {
         public Client Clients {get; set;}
 
-        public IEnumerable<Service> Services {get; set;}
-
         public Stylist Stylists {get; set;}
 
         public Appointment Appointment {get; set;}
 
         public ApplicationUser User { get; set; }
+        
+        public IEnumerable<Service> Services {get; set;}
     }
 }
